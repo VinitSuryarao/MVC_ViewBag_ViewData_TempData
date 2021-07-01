b187d57f4685923961e56a05478f54390bcf0ea0
Vinit Suryarao <suryarao.vinit@hotmail.com>
2021-07-01 10:55:07 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewBag_ViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.CompanyName = "Financial Technologies Ltd";
+            ViewBag.EmpList = new List<string> { "Ram","Sham","Raju" };
+
+            ViewData["CityName"] = "Panvel";
+            ViewData["CityList"] = new List<string> { "Rabale", "Airoli", "Thane" };
+            
+            return View();
+        }
+    }
+}
---
+++ Controllers/TempDataController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewBag_ViewData.Controllers
+{
+    public class TempDataController : Controller
+    {
+        // GET: TempData
+        public ActionResult Index1()
+        {
+            TempData["MyKey"] = " Data From Index 1";
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            ViewBag.Data = TempData.Peek("MyKey");
+            TempData.Keep("MyKey"); // TempData.Keep()
+            return View();
+        }
+
+        public ActionResult Index3()
+        {
+            ViewBag.Data = TempData.Peek("MyKey");
+            return View();
+        }
+    }
+}
